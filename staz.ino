700182f7fee96d7feb5ddcdba2829a23d01cfe99
Andrea Cosso <andrebog11@gmail.com>
2023-03-05 01:48:47 +0100

    primo prototipo software

--- staz.ino
+++ staz.ino
@@ -3,10 +3,10 @@
 #include "RTClib.h"
 #include "Adafruit_SHT31.h"
 #include "Adafruit_BMP085.h"
-
+#include <SoftwareSerial.h>
 
 #define seaLevelPressure_hPa 1013.25
-
+SoftwareSerial mySerial(3, 2);
 
 unsigned char temp = A0;
 unsigned int anem = 3;
@@ -19,143 +19,241 @@ float t1 = 0;
 float t2 = 0;
 float vel = 0;
 
+// CONTROLLI PER SENSORI
+bool term_control = true;
+bool rtc_control = true;
+bool bar_control = true;
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 RTC_DS1307 rtc;
 Adafruit_BMP085 bmp;
 
+// FUNZIONI ___________________________________________________________________________
+void anemometer()
+{
+    n++;
+    if (control)
+    {
+        t1 = millis() / 1000;
+        control = false;
+    }
+}
 
-//FUNZIONI ___________________________________________________________________________
+void updateSerial()
+{
+    delay(500);
+    while (Serial.available()) // CREDO CHE QUESTO NON SERVA A NIENTE
+    {
+        mySerial.write(Serial.read()); // Forward what Serial received to Software Serial Port
+    }
+    while (mySerial.available())
+    {
+        Serial.write(mySerial.read()); // Forward what Software Serial received to Serial Port
+    }
+}
 
-void anemometer(){
-  n++;
-  if(control){
-    t1 = millis()/1000;
-    control = false;
-  }
+void send_mess(const char s)
+{
+    mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+    updateSerial();
+    mySerial.println("AT+CMGS=\"+393890954340\""); // change ZZ with country code and xxxxxxxxxxx with phone number to sms
+    updateSerial();
+    mySerial.print(s); // text content
+    updateSerial();
+    mySerial.write(26); //è l equivalente di Ctrl+Z ogni cosa terminata da ctrl z è trattato come un messaggio
 }
 
 
-void setup(){
-  Serial.begin(9600);
+// DATA E ORA ****************************************************************
+void data_ora(){
+    if (rtc_control)
+    {
+        DateTime now = rtc.now();
 
-  //TERMOMETRO ****************************************************************
-  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate I2C address
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
-  }
-
-  //RTC ***********************************************************************
-  if (! rtc.begin()) { //la rtc ha range di lavoro 0-70 gradi, quindi nel casi si andasse sotto zero pesantemnte (inverno) come si fa?
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-
-  //ANEMOMETRO *******************************************************************
-  attachInterrupt(digitalPinToInterrupt(anem),anemometer,HIGH);
-
-  //BAROMETRO *********************************************************
-  if (!bmp.begin()) 
-  {
-	  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	  while (1) {}
-  }
-
+        Serial.print(now.day(), DEC);
+        Serial.print('/');
+        Serial.print(now.month(), DEC);
+        Serial.print('/');
+        Serial.print(now.year(), DEC);
+        Serial.print(" (");
+        Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+        Serial.print(") ");
+        Serial.print(now.hour(), DEC);
+        Serial.print(':');
+        Serial.print(now.minute(), DEC);
+        Serial.print(':');
+        Serial.print(now.second(), DEC);
+        Serial.print("\t");
+    }
 }
 
-void loop(){
+
+void setup()
+{
+    Serial.begin(9600);
+    mySerial.begin(9600);
+    delay(100);
+
+    // GSM
+    mySerial.println("AT"); // Once the handshake test is successful, it will back to OK
+    if (mySerial.read() != "OK")
+    {
+        Serial.print("puttana eva non funzia");
+    }
+    mySerial.println("AT+CSQ"); // Signal quality test, value range is 0-31 , 31 is the best
+    if (mySerial.read() < 5)
+    {
+        Serial.print("puttana eva il segnale è basso");
+    }
+    mySerial.println("AT+CCID"); // Read SIM information to confirm whether the SIM is plugged
+    /*
+    if(mySerial.read() != "OK"){               NON SO COME CAZZO CONTROLLARE SE FUNZIONA
+        Serial.print("puttana eva");
+    }
+    */
+    mySerial.println("AT+CREG?"); // Check whether it has registered in the network
+    if ((mySerial.read() != 1) || (mySerial.read() != 5))
+    {
+        Serial.print("puttana eva a che cazzo è collegato");
+    }
+
+    // TERMOMETRO ****************************************************************
+    if (!sht31.begin(0x44))
+    { // Set to 0x45 for alternate I2C address
+        send_mess("il termometro non va puttana eva");
+        term_control = false;
+    }
+
+    // RTC ***********************************************************************
+    /*
+    if (!rtc.begin()) { //la rtc ha range di lavoro 0-70 gradi, quindi nel casi si andasse sotto zero pesantemnte (inverno) come si fa?
+      send_mess("Couldn't find RTC");
+    }
+    */
+    if (!rtc.isrunning())
+    {
+        send_mess("RTC is NOT running!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        // This line sets the RTC with an explicit date & time, for example to set
+        // January 21, 2014 at 3am you would call:
+        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+        rtc_control = false;
+    }
+
+    // ANEMOMETRO *******************************************************************
+    attachInterrupt(digitalPinToInterrupt(anem), anemometer, HIGH);
+
+    // BAROMETRO *********************************************************
+    if (!bmp.begin())
+    {
+        send_mess("Could not find a valid BMP085 sensor, check wiring!");
+        bar_control = false;
+    }
+}
+
+void loop()
+{
+
+    float tAverage0 = millis();
+    float tempAverage = 0;
+    float humAverage = 0;
+    float pressAverage = 0;
+
+    int tempCount = 0;      //  MEGLIO USARE UN LONG?
+    int humCount = 0;
+    int pressCount = 0;
 
 
+    // TEMPERATURA E UMIDITA ***********************************************************
+    if (term_control)
+    {
 
-//DATA E ORA ****************************************************************
-  DateTime now = rtc.now();
+        float t = sht31.readTemperature();
+        float h = sht31.readHumidity();
 
-  Serial.print(now.day(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.year(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.print("\t");
+        if (!isnan(t))
+        { // check if 'is not a number'
+            //MEDIA
+            tempAverage += t;
+            tempCount++;
 
+            Serial.print("Temp *C = ");
+            Serial.print(t);
+            Serial.print("\t");
 
+        }
+        else
+        {
+            Serial.println("Failed to read temperature");
+            Serial.print("\t");
+        }
 
-//TEMPERATURA E UMIDITA ***********************************************************
-  float t = sht31.readTemperature();
-  float h = sht31.readHumidity();
+        if (!isnan(h))
+        { // check if 'is not a number'
+            humAverage += h;
+            humCount++;
 
-  if (! isnan(t)) {  // check if 'is not a number'
-    Serial.print("Temp *C = "); 
-    Serial.print(t); 
+            Serial.print("Hum. % = ");
+            Serial.println(h);
+            Serial.print("\t");
+        }
+        else
+        {
+            Serial.println("Failed to read humidity");
+            Serial.print("\n");
+        }
+    }
+
+    // ANEMOMETRO *******************************************************************
+
+    // a 2.4 km/h lo switch si chiude 1 volta al secondo
+    t2 = millis() / 1000;
+
+    if ((t2 - t1) >= 10)
+    {
+        count10 = n;
+        n = 0;
+        control = true;
+    }
+
+    count = count10 / (t2 - t1) // numero di volte in cui si chiude lo switch in un secondo
+    vel = 2.4 * count;
+
+    Serial.print("Velocita: ");
+    Serial.print(vel);
     Serial.print("\t");
 
-  } else { 
-    Serial.println("Failed to read temperature");
-    Serial.print("\t");
-  }
-  
-  if (! isnan(h)) {  // check if 'is not a number'
-    Serial.print("Hum. % = ");
-    Serial.println(h);
-    Serial.print("\t");
-  } else { 
-    Serial.println("Failed to read humidity");
-    Serial.print("\n");
-  }
+    // BAROMETRO *********************************************************
+    if (bar_control)
+    {
+        float pressure = bmp.readPressure();
+        if (!isnan(pressure))
+        {
+            pressAverage += pressure;
+            pressCount++;
 
+            Serial.print("Pressione: ");
+            Serial.print(pressure);
+            Serial.print("\t");
+        }
+        else
+        {
+            Serial.Print("Unable to read pressure");
+        }
+    }
 
-//ANEMOMETRO *******************************************************************
+    if((millis() - tAverage0) > 600000) //600000 sono 10 minuti
+    {
+        //MANDA I DATI
+        tempAverage = tempAverage / tempCount;
+        humAverage = humAverage / humCount;
+        pressAverage = pressAverage / pressCount;
+        data_ora();
 
-  //a 2.4 km/h lo switch si chiude 1 volta al secondo 
-  t2 = millis()/1000;
+        tAverage0 = millis();
+    }
+    
 
-  if((t2-t1) >= 10)
-  {
-    count10 = n;
-    n = 0;
-    control = true;
-  }
-
-  count = count10/(t2-t1) //numero di volte in cui si chiude lo switch in un secondo
-  vel = 2.4*count;
-
-  Serial.print("Velocita: ");
-  Serial.print(vel);
-  Serial.print("\t");
-
-//BAROMETRO *********************************************************
-  float pressure = bmp.readPressure();
-
-  if(!isnan(pressure))
-  {
-    Serial.print("Pressione: ");
-    Serial.print(pressure);
-    Serial.print("\t");
-  }
-  else
-  {
-    Serial.Print("Unable to read pressure");
-  }  
-  
-
-
-
-
-  delay(1000);
-
+    delay(200);
 }
